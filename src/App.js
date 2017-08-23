558dc1ab1d612dd2e4602bba956fe9435beacca1
sarahsweat <sarahsweat@gmail.com>
2017-08-23 17:02:22 -0400

    fixed team conditional on user page

--- src/App.js
+++ src/App.js
@@ -29,23 +29,21 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-     fetch(process.env.REACT_APP_API + '/users')
-     .then(data => data.json())
-     .then(users => {
-       this.setState({users, currentUsers: users})
-     })
+    fetch(process.env.REACT_APP_API + '/users')
+    .then(data => data.json())
+    .then(users => this.setState({users, currentUsers: users}))
 
-     fetch(process.env.REACT_APP_API + '/teams')
-     .then(data => data.json())
-     .then(teams => this.setState({teams, currentTeams: teams}))
+    fetch(process.env.REACT_APP_API + '/teams')
+    .then(data => data.json())
+    .then(teams => this.setState({teams, currentTeams: teams}))
 
-     fetch(process.env.REACT_APP_API + '/donations')
-     .then(data => data.json())
-     .then(donations => this.setState({donations}))
+    fetch(process.env.REACT_APP_API + '/donations')
+    .then(data => data.json())
+    .then(donations => this.setState({donations}))
 
-     fetch(process.env.REACT_APP_API + '/graph')
-     .then(data => data.json())
-     .then(data => this.setState({data}))
+    fetch(process.env.REACT_APP_API + '/graph')
+    .then(data => data.json())
+    .then(data => this.setState({data}))
   }
 
   handlePost = () => {
@@ -94,15 +92,14 @@ class App extends Component {
             <Route path='/' render={() => {return <NavBar/>}} />
             <Switch>
               <Route exact path='/' render={() => {return <HomeContainer users={this.state.users} teams={this.state.teams} donations={this.state.donations} handlePost={this.handlePost} data={this.state.data} />}}/>
-              <Route path='/donate' render={() => {return <DonateContainer handlePost={this.handlePost} users={this.state.users}/>}} />
-              <Route path='/signup' render={() => {return <SignupContainer teams={this.state.teams} handlePost={this.handlePost}/>}} />
+              <Route exact path='/donate' render={() => {return <DonateContainer handlePost={this.handlePost} users={this.state.users}/>}} />
+              <Route exact path='/signup' render={() => {return <SignupContainer teams={this.state.teams} handlePost={this.handlePost}/>}} />
               <Route exact path='/users' render={() => {return <UserContainer handleSearch={this.handleUserSearch} users={this.state.currentUsers}/>}} />
               <Route exact path='/teams' render={() => {return <TeamContainer handleSearch={this.handleTeamSearch} teams={this.state.currentTeams} handlePost={this.handlePost}/>}} />
               <Route path='/users/:id' render={() => {return <UserProfileContainer users={this.state.users} teams={this.state.teams} donations={this.state.donations} handlePost={this.handlePost} />}} />
               <Route path='/teams/:id' render={() => {return <TeamProfileContainer handlePost={this.handlePost} />}} />
-              <Route path='/map' render={() => {return <DonationMap />}}/>
-              <Route path='/milestones' render={() => {return <MilestonePageContainer users={this.state.users} data={this.state.data} />}} />
-              <Route path='/*' component={NotFound} />
+              <Route exact path='/milestones' render={() => {return <MilestonePageContainer users={this.state.users} data={this.state.data} />}} />
+              <Route component={NotFound} />
             </Switch>
           </div>
         </div>
--- src/components/users/UserProfileContainer.js
+++ src/components/users/UserProfileContainer.js
@@ -39,11 +39,10 @@ export default class UserProfileContainer extends React.Component {
                   {this.state.user.individual_total > 1000 ? <Header as='h2' className='comma-font' textAlign='center'>Comma Club Member!!</Header> : null }
                   <Divider/>
                   <UserEditForm handlePost={this.props.handlePost} handlePut={this.handlePut} teams={this.props.teams}/>
-                  <img src='../photos/comma.png'/>
                 </Grid.Column>
                 <Grid.Column>
                   <Header className='main-title' as='h1' textAlign='center'>{this.state.user.first_name} {this.state.user.last_name}</Header>
-                  <Header as='h1' textAlign='center'>Team: <Link to={`/teams/${this.state.user.team.id}`}> {this.state.user.team.name}</Link></Header>
+                  {this.state.user.team ? <Header as='h1' textAlign='center'>Team: <Link to={`/teams/${this.state.user.team.id}`}> {this.state.user.team.name}</Link></Header> : null }
                   <Header as='h4' textAlign='center'>{this.state.user.bio}</Header>
                   <Header as='h3' textAlign='center'>{this.state.user.first_name} has a goal of ${this.state.user.goal.toLocaleString()}  and has raised: ${this.state.user.individual_total.toLocaleString()} so far!</Header>
                   <Progress percent={Math.round((this.state.user.individual_total / this.state.user.goal)*100)} progress success />
